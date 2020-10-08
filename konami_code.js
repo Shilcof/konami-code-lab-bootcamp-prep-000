c9e9d6ef4ccfb051b743f525c8275500f2fdaccb
Robert Shilcof <robert.shilcof@gmail.com>
2020-10-08 12:45:12 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -18,5 +18,13 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
     // Now, how can we check for which specific key was pressed?
     
+    console.log(e.key);
+    
+    const input = document.querySelector('input');
+ 
+    input.addEventListener('keydown', function(e) {
+      console.log(e.key);
+    });
+    
   }
 }
